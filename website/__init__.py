0481d4177723d074c97e9e26518bb87cbd165d11
Gokulram A <40627011+GokulramGHV@users.noreply.github.com>
2021-11-13 22:59:28 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -35,9 +35,9 @@ def create_app():
     return app
 
 
-# def create_database(app):
-#     if not path.exists('website/' + DB_NAME):
-#         db.create_all(app=app)
-#         print('Created Database!')
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
 
 
